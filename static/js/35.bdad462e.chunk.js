541cbeb123dc5ae607d76c753fc5d6f940612c5a
Anastasiia-Svechnikova <105248135+Anastasiia-Svechnikova@users.noreply.github.com>
2022-09-27 07:58:19 +0000

    Deploying to gh-pages from @ Anastasiia-Svechnikova/redux-practice@134c801e3d41ea3dda98441c377676da9fb5aa21 🚀

---
+++ static/js/35.bdad462e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[35],{2035:function(e,t,r){r.r(t),r.d(t,{default:function(){return u}});var n=r(184),u=function(){return(0,n.jsx)("h2",{children:"Home"})}}}]);
+//# sourceMappingURL=35.bdad462e.chunk.js.map
